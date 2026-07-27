4bce6a9b77efa55072324f0cb549eab70978372f
antonino <antopata99@gmail.com>
2026-07-27 16:38:41 +0200

    Update angular.json, proxy.conf.json, and 7 more files...

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,39 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'games/:name',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile/:username',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'games',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'friends',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'signup',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+    canActivate: [guestGuard],
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./features/auth/signup/signup.component').then((m) => m.SignupComponent),
+    canActivate: [guestGuard],
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'games',
+    loadComponent: () =>
+      import('./features/games/games-list/games-list.component').then(
+        (m) => m.GamesComponent,
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'games/:name',
+    loadComponent: () =>
+      import('./features/games/game-detail/game-detail.component').then(
+        (m) => m.GameDetailComponent,
+      ),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'wishlist',
+    loadComponent: () =>
+      import('./features/wishlist/wishlist.component').then((m) => m.WishlistComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'friends',
+    loadComponent: () =>
+      import('./features/friends/friends.component').then((m) => m.FriendsComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile/:username',
+    loadComponent: () =>
+      import('./features/profile/profile.component').then((m) => m.ProfileComponent),
+    canActivate: [authGuard],
+  },
+  { path: '**', redirectTo: 'home' },
+];
