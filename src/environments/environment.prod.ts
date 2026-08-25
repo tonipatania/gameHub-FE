f9d214b1991a4dc43ebbd4b59573eafe7a63bacf
antonino <antopata99@gmail.com>
2026-08-25 15:02:24 +0200

    preparing file for deploy on verceò

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // Sostituisci con l'URL pubblico del backend dopo averlo deployato (es. Render),
+  // poi ricommitta: Vercel farà automaticamente un nuovo deploy con il valore aggiornato.
+  apiUrl: 'https://gamehub-kidi.onrender.com',
+};
